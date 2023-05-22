358bd04e386943abbb9b94fb20f5c1f31df890d0
Rafael Goncalves <rsgoncalves@gmx.com>
2023-05-22 12:35:20 -0400

    Add example function to search for resources mapped to multiple search terms

---
+++ src/query_database.py
@@ -0,0 +1,52 @@
+import sqlite3
+import pandas as pd
+
+
+def resources_annotated_with_term(db_cursor, search_terms, include_subclasses=True, direct_subclasses_only=False):
+    if include_subclasses:
+        if direct_subclasses_only:
+            ontology_table = "efo_edges"
+        else:
+            ontology_table = "efo_entailed_edges"
+    else:
+        ontology_table = "efo_edges"
+
+    query = '''SELECT DISTINCT 
+                    m.`STUDY.ACCESSION`,
+                    m.`DISEASE.TRAIT`,
+                    m.MAPPED_TRAIT,
+                    m.MAPPED_TRAIT_URI
+                FROM `gwascatalog_metadata` m
+                LEFT JOIN ''' + ontology_table + ''' ee ON (m.MAPPED_TRAIT_CURIE = ee.Subject)'''
+
+    index = 0
+    where_clause = "\nWHERE ("
+    for term in search_terms:
+        if index == 0:
+            where_clause += "m.MAPPED_TRAIT_CURIE = \'" + term + "\'"
+        else:
+            where_clause += " OR m.MAPPED_TRAIT_CURIE = \'" + term + "\'"
+        if include_subclasses:
+            where_clause += " OR ee.Object = \'" + term + "\'"
+        index += 1
+    query += where_clause + ")"
+
+    print(query + "\n")
+
+    results = db_cursor.execute(query).fetchall()
+    results_columns = [x[0] for x in db_cursor.description]
+    return pd.DataFrame(results, columns=results_columns)
+
+
+if __name__ == '__main__':
+    connection = sqlite3.connect("../gwascatalog_search.db")
+    cursor = connection.cursor()
+
+    # Search for GWASCatalog studies mapped to either EFO:0009605 (pancreas disease) or EFO:0005741 (infectious disease)
+    df = resources_annotated_with_term(db_cursor=cursor,
+                                       search_terms=['EFO:0009605', 'EFO:0005741'],
+                                       include_subclasses=True,
+                                       direct_subclasses_only=False)
+    print(df)
+    cursor.close()
+    connection.close()
