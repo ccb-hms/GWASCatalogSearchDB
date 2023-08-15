1173e7ec11968b492deb1ea1028e30bd098701d4
Rafael Goncalves <rsgoncalves@gmx.com>
2023-08-15 12:22:41 -0400

    Fixes issue with mismatching CURIEs that resulted in slight count discrepancy
    The "OBO" prefix used in the SemanticSQL tables is lower-case, so we changed the CURIEs coming from text2term/bioregistry to match

--- src/generate_ontology_tables.py
+++ src/generate_ontology_tables.py
@@ -5,7 +5,7 @@ import bioregistry
 import pandas as pd
 from collections import deque
 
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 
 SUBJECT_COL = "Subject"
 OBJECT_COL = "Object"
@@ -191,7 +191,10 @@ def _get_curie(term):
             return "DBR:" + term.rsplit('/', 1)[1]
         else:
             return term
-    return curie.upper()
+    curie = curie.upper()
+    if "OBO:" in curie:
+        curie = curie.replace("OBO:", "obo:")
+    return curie
 
 
 def _get_disease_location(connection, subject):
