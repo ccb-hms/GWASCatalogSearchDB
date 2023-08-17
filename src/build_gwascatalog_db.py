2dde906ee4689eacd1bc290f89b8dec6da85614f
Rafael Goncalves <rsgoncalves@gmx.com>
2023-08-17 19:02:26 -0400

    Fetch the studies metadata directly from GWASCatalog

--- src/build_gwascatalog_db.py
+++ src/build_gwascatalog_db.py
@@ -1,17 +1,58 @@
+import io
 import os
 import sys
 import time
 import pandas as pd
+import requests
 from generate_ontology_tables import get_curie_id_for_term
 
-__version__ = "0.3.1"
+__version__ = "0.4.0"
+
+GWASCATALOG_STUDIES_TABLE_URL = "https://www.ebi.ac.uk/gwas/api/search/downloads/studies_alternative"
+
+# Column names of the studies metadata table
+INPUT_METADATA_STUDY_ID_COLUMN = "STUDY ACCESSION"
+INPUT_METADATA_TABLE_TRAIT_COLUMN = "DISEASE/TRAIT"
+PUBMED_ID_COLUMN = "PUBMEDID"
+
+# Column names of ontology mapping details in input metadata (which are also used in the output database)
+MAPPED_TRAIT_COLUMN = "MAPPED_TRAIT"
+MAPPED_TRAIT_URI_COLUMN = "MAPPED_TRAIT_URI"
+MAPPED_TRAIT_CURIE_COLUMN = "MAPPED_TRAIT_CURIE"
+
+# Column names of the output database, which are different from the input as of the latest version
+OUTPUT_DB_STUDY_ID_COLUMN = "STUDY.ACCESSION"
+OUTPUT_DB_TRAIT_COLUMN = "DISEASE.TRAIT"
+
+
+def download_gwascatalog_table(table_url):
+    response = requests.get(table_url)
+    if response.status_code == 200:
+        data = response.content
+        df = pd.read_csv(io.StringIO(data.decode('utf-8')), sep="\t")
+        return df
+    else:
+        print(f"Failed to retrieve GWAS Catalog table from the URL {table_url}")
+
+
+def get_gwascatalog_studies_table():
+    gwascatalog_studies_df = download_gwascatalog_table(GWASCATALOG_STUDIES_TABLE_URL)
+    gwascatalog_studies_df = gwascatalog_studies_df.drop(gwascatalog_studies_df.columns[0], axis=1)
+    gwascatalog_studies_df[MAPPED_TRAIT_CURIE_COLUMN] = gwascatalog_studies_df[MAPPED_TRAIT_URI_COLUMN].apply(
+        get_curie_id_for_term)
+
+    # In studies v1.0.2 the names of columns changed w.r.t. the previous table
+    gwascatalog_studies_df = gwascatalog_studies_df.rename(
+        columns={INPUT_METADATA_STUDY_ID_COLUMN: OUTPUT_DB_STUDY_ID_COLUMN,
+                 INPUT_METADATA_TABLE_TRAIT_COLUMN: OUTPUT_DB_TRAIT_COLUMN})
+    gwascatalog_studies_df.to_csv("../resources/gwascatalog_metadata.tsv", sep="\t", index=False)
+    return gwascatalog_studies_df
 
 
 def get_text2term_mappings_table(metadata_df):
     mappings_list = []
     for _, row in metadata_df.iterrows():
-        # Split the comma-separated URIs in "MAPPED_TRAIT_URI" into separate rows
-        mapped_trait_uri = row['MAPPED_TRAIT_URI']
+        mapped_trait_uri = row[MAPPED_TRAIT_URI_COLUMN]
         # TODO split the comma-separated labels as well, or obtain the label for each split out IRI
         if mapped_trait_uri != "" and not pd.isna(mapped_trait_uri):
             if "," in mapped_trait_uri:
@@ -21,11 +62,11 @@ def get_text2term_mappings_table(metadata_df):
             # Add a new row to the "mappings_df" for each IRI in the list
             for iri in iris_list:
                 iri = iri.strip()
-                mappings = {'STUDY.ACCESSION': row['STUDY.ACCESSION'],
-                            'DISEASE.TRAIT': row['DISEASE.TRAIT'],
-                            'MAPPED_TRAIT': row['MAPPED_TRAIT'],
-                            'MAPPED_TRAIT_URI': iri,
-                            'MAPPED_TRAIT_CURIE': get_curie_id_for_term(iri)}
+                mappings = {OUTPUT_DB_STUDY_ID_COLUMN: row[OUTPUT_DB_STUDY_ID_COLUMN],
+                            OUTPUT_DB_TRAIT_COLUMN: row[OUTPUT_DB_TRAIT_COLUMN],
+                            MAPPED_TRAIT_COLUMN: row[MAPPED_TRAIT_COLUMN],
+                            MAPPED_TRAIT_URI_COLUMN: iri,
+                            MAPPED_TRAIT_CURIE_COLUMN: get_curie_id_for_term(iri)}
                 mappings_list.append(mappings)
     mappings_df = pd.DataFrame(mappings_list)
     mappings_df.to_csv("../resources/gwascatalog_mappings.tsv", sep="\t", index=False)
@@ -33,9 +74,8 @@ def get_text2term_mappings_table(metadata_df):
 
 
 if __name__ == "__main__":
-    gwascatalog_metadata = pd.read_csv("../resources/gwascatalog_metadata.tsv", sep="\t")
-    gwascatalog_metadata = gwascatalog_metadata.drop(gwascatalog_metadata.columns[0], axis=1)
-    gwascatalog_metadata["MAPPED_TRAIT_CURIE"] = gwascatalog_metadata["MAPPED_TRAIT_URI"].apply(get_curie_id_for_term)
+    print("Downloading GWAS Catalog studies table...")
+    gwascatalog_metadata = get_gwascatalog_studies_table()  # get studies metadata table
 
     # Generate and save a text2term-formatted table of ontology mappings in the GWAS Catalog metadata table
     ontology_mappings = get_text2term_mappings_table(gwascatalog_metadata)
@@ -47,7 +87,6 @@ if __name__ == "__main__":
     else:
         print("NCBI API Key not provided—PubMed queries will be slower. Provide API Key as a parameter to this module.")
 
-    # Build the database
     start = time.time()
     from build_database import build_database
     build_database(dataset_name="gwascatalog",
@@ -55,8 +94,8 @@ if __name__ == "__main__":
                    ontology_mappings_df=ontology_mappings,
                    ontology_name="EFO",
                    ontology_url="http://www.ebi.ac.uk/efo/releases/v3.43.0/efo.owl",
-                   resource_col="DISEASE.TRAIT",
-                   resource_id_col="STUDY.ACCESSION",
-                   ontology_term_iri_col="MAPPED_TRAIT_URI",
-                   pmid_col="PUBMEDID")
+                   resource_col=OUTPUT_DB_TRAIT_COLUMN,
+                   resource_id_col=OUTPUT_DB_STUDY_ID_COLUMN,
+                   ontology_term_iri_col=MAPPED_TRAIT_URI_COLUMN,
+                   pmid_col=PUBMED_ID_COLUMN)
     print(f"Finished building database ({time.time() - start:.1f} seconds)")
